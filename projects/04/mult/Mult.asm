189e96d014343f1dc354c558051ff5e6522178b5
Vangelis <vangelisandr@gmail.com>
2024-01-19 18:28:16 +0200

    Fixed mult, tested and passed

--- projects/04/mult/Mult.asm
+++ projects/04/mult/Mult.asm
@@ -6,39 +6,116 @@
 // Multiplies R0 and R1 and stores the result in R2.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[3], respectively.)
 
-  // Initialize number to
-  @R0
-  D=M
-  @number
-  M=D
-
-  // Initialize m to 0
+  // Initialize i to 0
   @0
   D=A
-  @m
+  @i
   M=D
 
-  // Initialize RAM[3] = 0
-  @R3
+  // Initialize RAM[2] = 0
+  @R2
   M=0
 
-(MULTIPLIER_LOOP)
-  // if m - RAM[1] == 0 goto END else multiply
-  @m
+  // Check if any of the the two numbers is 0
+  @R0
+  D=M
+  @ZERO
+  D;JEQ
+  @R1
+  D=M
+  @ZERO
+  D;JEQ
+
+  // Check if one of the numbers is negative
+  @R0
+  D=M
+  @R0_NEGATIVE
+  D;JLT
+  @R1
+  D=M
+  @R1_NEGATIVE
+  D;JLT
+
+  // goto both positive
+  @BOTH_POSITIVE_MULT
+  0;JMP
+
+(R0_NEGATIVE)
+  // Check if the other is negative too and if so goto BOTH_NEGATIVE
+  @R1
+  D=M
+  @BOTH_NEGATIVE
+  D;JLT
+
+  // Negate R0
+  @R0
+  M=-M
+(R01_NEGATIVE_MULT)  
+  // if i - RAM[1] == 0 goto NEGATE else keep multiplying
+  @i
+  D=M
+  @R1
+  D=D-M
+  @NEGATE
+  D;JEQ
+
+  @R0
+  D=M
+  @R2
+  M=M+D
+  @i
+  M=M+1
+  @R01_NEGATIVE_MULT
+  0;JMP
+
+(R1_NEGATIVE)
+  // Check if the other is negative too and if so goto BOTH_NEGATIVE
+  @R0
+  D=M
+  @BOTH_NEGATIVE
+  D;JLT
+
+  // Negate R1
+  @R1
+  M=-M
+  @R01_NEGATIVE_MULT
+  0;JMP
+
+(NEGATE)
+  // Negate and goto END
+  @R2
+  M=-M
+  @END
+  0;JMP
+
+(BOTH_NEGATIVE)
+  // Negate both numbers
+  @R0
+  M=!M
+  @R1
+  M=!M
+
+(BOTH_POSITIVE_MULT)
+  // if i - RAM[1] == 0 goto END
+  @i
   D=M
   @R1
   D=D-M
   @END
   D;JEQ
 
-  @number
+  @R0
   D=M
-  @R3
+  @R2
   M=M+D
-  @m
+  @i
   M=M+1
-  @MULTIPLIER_LOOP
+  @BOTH_POSITIVE_MULT
   0;JMP
+
+(ZERO)
+  @R2
+  M=0
 
 (END)
   @END
